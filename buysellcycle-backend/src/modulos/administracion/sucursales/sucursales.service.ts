e44e97509260ba8899c91eaf5c3347f1ccb0c427
Juan <juanchoto94@gmail.com>
2026-09-18 21:13:25 -0300

    Sucursales services

--- buysellcycle-backend/src/modulos/administracion/sucursales/sucursales.service.ts
+++ buysellcycle-backend/src/modulos/administracion/sucursales/sucursales.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../../prisma/prisma.service.js'
 import { CreateSucursaleDto } from './dto/create-sucursale.dto.js';
 import { UpdateSucursaleDto } from './dto/update-sucursale.dto.js';
 
 @Injectable()
 export class SucursalesService {
-  create(createSucursaleDto: CreateSucursaleDto) {
-    return 'This action adds a new sucursale';
+  constructor (private readonly prisma: PrismaService) {}
+
+  async create(createSucursaleDto: CreateSucursaleDto) {
+    return await this.prisma.sucursal.create({
+      data: createSucursaleDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all sucursales`;
+  async findAll() {
+    return await this.prisma.sucursal.findMany({
+      where: { archivado: false },
+      include: {
+        provioncia: true,
+        localidad: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} sucursale`;
+  async findOne(id: number) {
+    const registro = await this.prisma.sucursal.findFirst({
+      where: {
+        id, 
+        archivado: false
+      },
+      include: {
+        provincia: true,
+        localidad: true,
+      },
+    });
+
+    if(!registro) {
+      throw new NotFoundException(`Sucursal con id #${id} no encontrada`);
+    }
+
+    return registro;
   }
 
-  update(id: number, updateSucursaleDto: UpdateSucursaleDto) {
-    return `This action updates a #${id} sucursale`;
+  async update(id: number, updateSucursaleDto: UpdateSucursaleDto) {
+    await this.findOne(id);
+
+    return await this.prisma.sucursal.update({
+      where: { id },
+      data: updateSucursaleDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} sucursale`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return await this.prisma.sucursal.update({
+      where: { id },
+      data: { archivado: true },
+    });
   }
 }
